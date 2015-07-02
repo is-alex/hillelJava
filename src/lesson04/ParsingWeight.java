68c567306744dca7218edb3340bd605ebe3dee4f
is-alex <alex@mbox.se>
2015-07-02 20:56:45 +0300

    alpha version

--- src/lesson04/ParsingWeight.java
+++ src/lesson04/ParsingWeight.java
@@ -1,13 +1,16 @@
 package lesson04;
 
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class ParsingWeight {
     public static void main(String[] args) {
 
-        String input = InputChecker();
+        String input = AskUnits();
         String string [] = input.split("\\.");
+
 
         if (string.length > 0) {
 
@@ -56,10 +59,8 @@ public class ParsingWeight {
                     System.out.print(" micrograms ");
                 } else if (i == 8) {
                     System.out.print(" nanograms ");
-                } else if (i == 11) {
+                } else if ((i > 8) && (i == (fractionLength - 1))) {
                     System.out.print(" picograms ");
-                } else if ((i > 11) && (i == (fractionLength - 1))) {
-                    System.out.print(" femtograms ");
                 }
 
             }
@@ -76,7 +77,7 @@ public class ParsingWeight {
 
         do {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Please, input weight (grams):");
+            System.out.println("Please, input weight:");
             if (scanner.hasNextBigDecimal()) {
                 n = scanner.nextBigDecimal();
                 if (n.signum() < 0) {
@@ -90,6 +91,56 @@ public class ParsingWeight {
         }
         while (resumeLoop);
         return String.valueOf(n);
+
+    }
+
+    public static String AskUnits () {
+
+        String inputChecker = InputChecker();
+
+        Scanner scanner = new Scanner(System.in);
+        String [] [] units = new String [10] [2];
+        units [0] [0] = "fg";
+        units [1] [0] = "pg";
+        units [2] [0] = "ng";
+        units [3] [0] = "mkg";
+        units [4] [0] = "mg";
+        units [5] [0] = "gr";
+        units [6] [0] = "kg";
+        units [7] [0] = "t";
+        units [8] [0] = "kt";
+        units [9] [0] = "mt";
+
+        for (int i = 0; i<units.length; i++) {
+            BigDecimal j = new BigDecimal(1000).pow(i);
+            units[i][1] = (((new BigDecimal(0.000000000000001).multiply(j))).setScale(8, RoundingMode.HALF_DOWN).toString());
+        }
+
+
+        String input;
+        boolean resumeLoop = true;
+        int index = 0;
+            do {
+
+                System.out.println("Please, input a unit of mass (\"fg\", \"pg\", \"ng\", \"mkg\"," +
+                            " \"mg\", \"gr\", \"kg\", \"t\", \"kt\", \"mt\"):");
+
+                input = scanner.next();
+                for (int j=0; j < units.length; j++) {
+
+                    if (input.matches(units[j][0])) {
+                        index = j;
+                        resumeLoop = false;
+                    }
+
+                }
+
+            } while (resumeLoop);
+
+
+         input = ((new BigDecimal(inputChecker)).multiply(new BigDecimal(units [index][1]))).toString();
+
+        return input;
 
     }
 
