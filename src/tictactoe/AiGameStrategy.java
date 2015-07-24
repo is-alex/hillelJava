97f5539713cda6c99928af2cc679009475a5d369
is-alex <alex@mbox.se>
2015-07-24 18:04:52 +0300

    a draft; doesn't work

---
+++ src/tictactoe/AiGameStrategy.java
@@ -0,0 +1,8 @@
+//package tictactoe;
+//
+//
+//public interface AiGameStrategy {
+//    public int aiMakeTurn(int[] field);
+//
+//
+//}
---
+++ src/tictactoe/RandomAiGameStrategy.java
@@ -0,0 +1,15 @@
+//package tictactoe;
+//
+//
+//
+//public class RandomAiGameStrategy implements AiGameStrategy {
+//    @Override
+//    public int aiMakeTurn(int[] field) {
+//        while(true){
+//            int randomPlace = (int)(Math.random()*9);
+//            if(field[randomPlace]==0){
+//                return randomPlace;
+//            }
+//        }
+//    }
+//}
