94e7d75ada45726cf133db6bacd3b2f43142c92b
is-alex <alex@mbox.se>
2015-07-28 02:00:46 +0300

    new methods added

---
+++ src/mytreeset/TestApp.java
@@ -0,0 +1,52 @@
+package mytreeset;
+
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+public class TestApp {
+    public static void main(String[] args) {
+        MyTreeSet tree = new MyTreeSet();
+        BigDecimal a = new BigDecimal(10.25);
+        BigDecimal b = new BigDecimal(-2015);
+        BigDecimal c = new BigDecimal(-40004);
+        BigDecimal d = new BigDecimal(800.5);
+        BigDecimal e = new BigDecimal(-1562);
+        BigDecimal f = new BigDecimal(1098);
+        BigDecimal g = new BigDecimal(111112);
+        BigDecimal h = new BigDecimal(-1);
+
+        tree.add(a);
+        tree.add(b);
+//        tree.add(c);
+        tree.add(d);
+        tree.add(e);
+        tree.add(f);
+        tree.add(g);
+        tree.add(h);
+
+//        int testInt =34;
+//        HashMap hashMap = new HashMap();
+
+//        tree.add(testInt);  //testing exceptions
+//        tree.add(hashMap);   //testing exceptions
+
+//        System.out.println(tree.contains(testInt));  //testing exceptions
+//        System.out.println(tree.contains(hashMap));  //testing exceptions
+
+        System.out.println("Contains " + a.toString() + ": " + tree.contains(a));
+        System.out.println("Contains " + b.toString() + ": " + tree.contains(b));
+        System.out.println("Contains " + c.toString() + ": " + tree.contains(c));
+        System.out.println();
+        System.out.println(tree.toString());
+        System.out.println("Min: " + tree.getMin());
+        System.out.println("Max: " + tree.getMax());
+        System.out.println("Size: " + tree.size());
+
+        BigDecimal d1 = new BigDecimal(800.6);
+        System.out.println("Floor: " + tree.floor(d1));
+        System.out.println("Ceiling: " + tree.ceiling(d1));
+
+    }
+
+}
