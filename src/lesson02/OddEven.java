261f70d4ad26288901296222f829b0f06b0902f1
is-alex <alex@mbox.se>
2015-08-13 21:38:56 +0300

    refactoring

--- src/lesson02/OddEven.java
+++ src/lesson02/OddEven.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class OddEven {
     public static void main(String[] args) {
 
-        int n = 0;
+        int n = inputChecker();
+        if (n%2==0) {
+            System.out.println(n + " is an even  number");
+        } else {
+            System.out.println(n + " is an odd  number");
+        }
 
+    }
+
+    private static int inputChecker(){
+        int number = 0;
         Scanner scanner = new Scanner(System.in);
         boolean resumeLoop = true;
         do {
             System.out.println("Please, input a number:");
             if (scanner.hasNextInt()) {
-                n = scanner.nextInt();
+                number = scanner.nextInt();
                 resumeLoop = false;
             } else {
                 System.out.println("Incorrect entry " + scanner.next() + ". Try again.");
             }
         }
         while (resumeLoop);
-
-        if (n%2==0) {
-            System.out.println(n + " is an even  number");
-        } else {
-            System.out.println(n + " is an odd  number");
-        }
         scanner.close();
-        System.exit(0);
+        return number;
     }
+
 }
