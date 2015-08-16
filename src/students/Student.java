11efd83482a0e1b612404d4130a0c0213d05a63e
is-alex <alex@mbox.se>
2015-08-16 23:26:31 +0300

    new

--- src/students/Student.java
+++ src/students/Student.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 public class Student implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private int id;
     private String surname;
     private int[] marks;
