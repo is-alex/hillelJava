9ff18a1127b5e9c871156197da4230cfd5ad2c1e
is-alex <alex@mbox.se>
2015-08-17 23:31:06 +0300

    new

--- src/students_io/Output.java
+++ src/students_io/Output.java
@@ -1,5 +1,39 @@
 package students_io;
 
 
+import students.Student;
+
+import java.io.*;
+
 public class Output {
+
+    private String filePath;
+
+    public Output(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+    public void write(Student student) throws IOException {
+        File file = new File(filePath);
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+            oos.writeObject(student);
+            oos.flush();
+        } catch(IOException e) {
+            System.err.println("Can't write to disk!");
+            e.printStackTrace();
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+        }
+    }
 }
