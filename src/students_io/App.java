36699d44fd2e5eedfa0cf42ae9ccdb1dc405120a
is-alex <alex@mbox.se>
2015-08-18 01:10:28 +0300

    creates a <random student id>.dat file

--- src/students_io/App.java
+++ src/students_io/App.java
@@ -1,30 +1,10 @@
 package students_io;
 
 
-import maptesting.MapTest;
-import students.Student;
-
 import java.io.*;
 
 public class App {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String filePath = "student.dat";
-        int minSurnameLength = 1;
-        int maxSurnameLength = 50;
-        int minNumOfMarks = 0;
-        int maxNumOfMarks = 35;
-
-        Output output = new Output(filePath);
-
-        Student student = new Student(1,
-                    MapTest.randomSurname(minSurnameLength, maxSurnameLength),
-                    MapTest.randomMarks(minNumOfMarks, maxNumOfMarks));
-
-            output.write(student);
-
-        Input input = new Input(filePath);
-        System.out.printf(input.read().toString());
-
-
+        Test test = new Test();
     }
 }
