34ffb361741639fa09adf76b05273bee3ad9aaa5
is-alex <alex@mbox.se>
2015-08-19 22:43:12 +0300

    homework

---
+++ src/serialization_gamesaving/GameCharacter.java
@@ -0,0 +1,51 @@
+package serialization_gamesaving;
+
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class GameCharacter implements Serializable {
+    private int power;
+    private String name;
+    private CharacterType type;
+    transient private String[] weapons;
+    public GameCharacter(int power, String name, CharacterType type, String[] weapons) {
+        this.power = power;
+        this.name = name;
+        this.type = type;
+        this.weapons = weapons;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPower() {
+        return power;
+    }
+    public void setPower(int power) {
+        this.power = power;
+    }
+    public CharacterType getType() {
+        return type;
+    }
+    public void setType(CharacterType type) {
+        this.type = type;
+    }
+    public String[] getWeapons() {
+        return weapons;
+    }
+    public void setWeapons(String[] weapons) {
+        this.weapons = weapons;
+    }
+    @Override
+    public String toString() {
+        return "GameCharacter{" +
+                "power=" + power +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                ", weapons=" + Arrays.toString(weapons) +
+                '}';
+    }
+}
