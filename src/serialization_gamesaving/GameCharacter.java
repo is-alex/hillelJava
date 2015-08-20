14ecabb1149bdd96b6ca853da918d40c08af47db
is-alex <alex@mbox.se>
2015-08-20 19:21:00 +0300

    homework

--- src/serialization_gamesaving/GameCharacter.java
+++ src/serialization_gamesaving/GameCharacter.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 public class GameCharacter implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int power;
     private String name;
     private CharacterType type;
