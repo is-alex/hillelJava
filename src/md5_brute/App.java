4afa8cd63aca3ed5fd13f4f05e31c88c21785201
is-alex <alex@mbox.se>
2015-09-03 23:46:43 +0300

    test

--- src/md5_brute/App.java
+++ src/md5_brute/App.java
@@ -1,4 +1,152 @@
 package md5_brute;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.concurrent.*;
+
 public class App {
+    private static long start = System.currentTimeMillis();
+    private static long mainStart = System.currentTimeMillis();
+    private final static char[] CH = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    //private final static char[] CH = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    // private final static char[] CH = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    //private final static char[] CH = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!№;%:?*()_-+=~`.,/".toCharArray();
+    //private final static char[] CH = "abcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_-+=~`.,/".toCharArray();
+    private static int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
+    private static long count = 0;
+    private static int loopCount = 0;
+    private static final long PRINT_PERIOD = 1_000_000;
+    private static int minLength = 3;
+    private static int maxLength = 4;
+    private static long size = estimateCombinations(maxLength);
+    private static ArrayBlockingQueue<String> generatedWords = new ArrayBlockingQueue<>(NUMBER_OF_CORES * 8000);
+    public static volatile boolean isRunning = true;
+    public static volatile boolean isProduced = false;
+
+
+    public static void main(String[] args) {
+        String md5 = "f016441d00c16c9b912d05e9d81d894d";
+        //String md5 = "5ebe2294ecd0e0f08eab7690d2a6ee69";
+        //String md5 = "13d70e09909669272b19647c2a55dacb";
+        //String md5 = "5f50dfa5385e66ce46ad8d08a9c9be68";
+
+        System.out.println("Number of possible combinations: " + size);
+        ExecutorService producerExecutorService = Executors.newSingleThreadExecutor();
+        ExecutorService consumerExecutorService = Executors.newCachedThreadPool();
+
+        Runnable producer = () -> {
+                System.out.println("Producer: " + Thread.currentThread().getName());
+                generateWords(CH, minLength, maxLength, generatedWords);
+            isProduced = true;
+        };
+        producerExecutorService.execute(producer);
+
+        Runnable consumer = () -> {
+            String testWord = null;
+            String result = null;
+            do {
+                try {
+                    testWord = generatedWords.take();
+                    // System.out.println(Thread.currentThread().getName()+"generatedWords.take() " + testWord);
+                } catch (InterruptedException e) {
+                    if (!isRunning && Thread.currentThread().isInterrupted())  //FIXME?
+                    return;
+                }
+                result = hash(testWord);
+
+                if (result.equals(md5)) {
+                    long totalTime = System.currentTimeMillis() - mainStart;
+                    System.out.println("Consumer: " + Thread.currentThread().getName() +
+                            "; Result found in: " + totalTime + " ms. Password is: " + testWord + ", hash: " + result);
+
+                    producerExecutorService.shutdownNow();
+                    consumerExecutorService.shutdownNow();
+                    isRunning = false;
+                    return;
+                } else if (isProduced && generatedWords.isEmpty()){
+                    System.out.println("No matches.");
+                }
+
+            } while (isRunning);
+        };
+
+        for (int i = 0; i < (NUMBER_OF_CORES * 8); i++) {
+            consumerExecutorService.execute(consumer);
+        }
+
+    }
+
+    public static void generateWords(char[] input, int minLength, int maxLength, ArrayBlockingQueue<String> queue) {
+
+        char[] result = new char[input.length];
+        int[] index = new int[maxLength];
+        Arrays.fill(result, 0, result.length, input[0]);
+        Arrays.fill(index, 0, index.length, 0);
+
+        for (int length = minLength; length <= maxLength; length++) {
+            int updateIndex = 0;
+            do {
+
+                try {
+                    queue.put(new String(result, 0, length));
+                    //System.out.println((new String(result, 0, length)));
+                    printStatus();
+                } catch (InterruptedException e) {
+                    if (!isRunning && Thread.currentThread().isInterrupted()) ;
+                    return; //FIXME!
+                }
+
+                for (updateIndex = (length - 1); (updateIndex != -1) && ++index[updateIndex] == input.length;
+                     result[updateIndex] = input[0], index[updateIndex] = 0, updateIndex--)
+                    ;
+
+                if (updateIndex != -1) result[updateIndex] = input[index[updateIndex]];
+            }
+            while (updateIndex != -1);
+        }
+    }
+
+
+    private static String hash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            StringBuilder hashtext = new StringBuilder();
+            hashtext.append(number.toString(16));
+            while (hashtext.length() < 32)
+                hashtext.append("0");
+            return hashtext.toString().toLowerCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printStatus() {
+        count++;
+        if (count % PRINT_PERIOD == 0) {
+            long duration = System.currentTimeMillis() - start;
+            long speed = (PRINT_PERIOD / duration) * 1000;
+            long remainingHours = size / speed / 3600;
+
+            System.out.println(count + " items processed in " + duration + " ms. Speed: " + speed + " p/s. Est. time remaining "
+                    + remainingHours + " h.");
+            start = System.currentTimeMillis();
+
+            System.out.println("current queue size: " + generatedWords.size());
+        }
+    }
+
+    private static long estimateCombinations(int length) {
+        long result = 0;
+        for (int i = minLength; i <= length; i++)
+            result += Math.pow(CH.length, i);
+        return result;
+    }
+
 }
+
+
+
