7973e11c586145293bae18059077460c58325c2f
is-alex <alex@mbox.se>
2015-09-14 14:03:15 +0300

    add

--- src/students/Students_App.java
+++ src/students/Students_App.java
@@ -1,15 +1,30 @@
 package students;
+import maptesting.MapTest;
+
+import java.security.SecureRandom;
 import java.sql.*;
+import java.util.Arrays;
 
 public abstract class Students_App {
    private static final String JDBC_DRIVER = "org.postgresql.Driver";
    private static final String DB_URL = "jdbc:postgresql://localhost:5432/students";
    private static final String DB_USERNAME = "postgres";
    private static final String DB_PASSWORD = "1";
+   private static final int minSurnameLength = 1;
+   private static final int maxSurnameLength = 50;
+   private static final  int minNumOfMarks = 0;
+   private static final  int maxNumOfMarks = 35;
 
     public static void main(String[] args) {
         try {
             createTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Student student = createRandomStudent();
+        try {
+            saveStudentToDB(student);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -57,5 +72,39 @@ public abstract class Students_App {
                 connection.close();
             }
         }
+    }
+
+    private static void saveStudentToDB (Student student) throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        String saveStudent = "INSERT INTO students (id, surname, marks) VALUES \n"
+                +"("+student.getId()
+                +", '"+student.getSurname()+"', '"
+                +Arrays.toString(student.getMarks())+"');";
+
+        try {
+            connection = getConnection();
+            statement = connection.createStatement();
+
+            statement.execute(saveStudent);
+            System.out.println("Inserted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    private static Student createRandomStudent(){
+        SecureRandom random = new SecureRandom();
+        return new Student(random.nextInt(Integer.MAX_VALUE),
+                MapTest.randomSurname(minSurnameLength, maxSurnameLength),
+                MapTest.randomMarks(minNumOfMarks, maxNumOfMarks));
     }
 }
